14f750d03528ba4309a785dbd1fc2638c6e4ec9a
Simon Hrouda <75166353+phuid@users.noreply.github.com>
2022-10-25 20:47:33 +0200

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+// const puck_width = 40;
+// const puck_height = 20;
+// const 
+
+var c = document.getElementById("fieldcanvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 500, 500);
+ctx.fillStyle = "#000065";
+ctx.fillRect(2000, 2000, 500, 500);
+ctx.stroke();
